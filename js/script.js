16eadc9d008f7511b6597a60c7951b03cc999739
Bruno <bruno2014mineiro@gmail.com>
2022-03-24 18:20:50 -0300

    Calculadora

--- js/script.js
+++ js/script.js
@@ -4,16 +4,56 @@ const tema = document.querySelector('#tema');
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
 const botao = document.querySelectorAll('.botao');
-let bloqueado = true;
+const tippyTema = document.querySelector('.tippy-tema')
 let verificado = false;
-let virgulaONOFF = false;
+let pontoONOFF = false;
+let bloqueado = false;
+let checar = false;
+let check = false;
+
+function mensagemUnica() {
+            tippy(tippyTema, {
+            content: "Clique aqui para mudar o tema da calculadora",
+            showOnCreate: true,
+            delay: [1000],
+            placement: 'top',
+            arrow: true,
+            animation: 'fade',
+            trigger: 'click',
+        });
+}
+function TemaClaro() {
+    tippy('#muda-tema', {
+    content: "Tema Claro Ativado!",
+    showOnCreate: true,
+    placement: 'top',
+    arrow: true,
+    animation: 'fade',
+    trigger: 'manual',
+    });
+}
+
+function TemaEscuro() {
+    tippy('#muda-tema', {
+    content: "Tema Escuro Ativado!",
+    showOnCreate: true,
+    placement: 'top',
+    arrow: true,
+    animation: 'fade',
+    trigger: 'manual',
+    });
+}
 
 function erro() {
-    Swal.fire({
-        icon: 'error',
-        title: 'ERRO!',
-        text: 'Digite um número!',
-    })
+    tippy('#Tela', {
+    content: "Insira um número!",
+    showOnCreate: true,
+    placement: 'top-end',
+    arrow: true,
+    theme: 'erro',
+    animation: 'fade',
+    trigger: 'manual',
+    });
 }
 
 function calcular(){
@@ -21,27 +61,61 @@ function calcular(){
         erro();
         return;
     }
-    if (bloqueado == true || verificado == false) {    
+    if (verificado == false) {
+        return;    
+    }
+    let conta = new Function("return " + telaM.value + tela.value);
+    let resultado = conta();
+    let resultadoFormatado = resultado.toString();
+    if (resultadoFormatado.indexOf(".") >= 1) {
+        console.log(resultado);
+        console.log(tela.value.indexOf(".") + ' Posicao do . Verificado');
+        telaM.value += tela.value;
+        Math.round(resultado);
+        console.log(Math.round(resultado) + ' ---- ')
+        tela.value = resultado.toFixed(2);
+        bloqueado = true;
+        verificado = false;
+        pontoONOFF = false;
     } else {
-        let conta = new Function("return " + telaM.value + tela.value);
-        let resultado = conta();
         console.log(resultado)
         console.log(tela.value)
-        telaM.value += tela.value
+        telaM.value += tela.value;
         tela.value = resultado;
         bloqueado = true;
         verificado = false;
+        pontoONOFF = false;
+        Math.round(resultado);
+        console.log(Math.round(resultado) + ' ---- ')
         console.log(tela.value.length);
-    }
+        console.log(tela.value.indexOf(".") + ' Posicao do .');
+        }
 }
 
 function adicionar(texto) {
+    //const stringNumero = tela.value.toString();
+    if (tela.value.indexOf(".") == -1 && pontoONOFF == true)
+    {
+        pontoONOFF = false;
+    }
+    if (pontoONOFF == true && texto == '.') {
+        console.log(tela.value.indexOf(".")); 
+        return;    
+    }
+    if (tela.value.indexOf(".") > 0) {
+        console.log(tela.value.indexOf("."));
+        pontoONOFF = true;
+        console.log(pontoONOFF);
+    }
     if (bloqueado == true) {
         telaM.value = '';
         tela.value += texto;
-        bloqueado = false; 
+        bloqueado = false;
+        console.log(bloqueado) 
     } else {
-        tela.value += texto; 
+        tela.value += texto;
+        console.log(pontoONOFF);
+        console.log(tela.value.indexOf("."));
     }
 }
 
@@ -59,28 +133,36 @@ function apagar() {
     }
 }
 
-function verificar() {
+function verificar(texto) {
+    if (tela.value.length > 1 && texto == '.') {
+        pontoONOFF = true;
+        console.log('oi')
+        console.log(tela.value.length + ' Valor da tela');
+        return
+    }
+    /*if (checar == true && texto == '+') {
+        let conta = new Function("return " + telaM.value + tela.value);
+        let resultado = conta();
+        telaM.value = resultado;
+        checar = false;
+    }*/
     if (tela.value.length == 1) {
+        console.log(tela.value.length + ' Valor da tela');
         tela.value = '';
         erro();
     } else {
+        console.log(tela.value.length + ' Valor da tela');
         telaM.value += tela.value;
         tela.value = '';
         verificado = true;
+        //checar = true;
         console.log(tela.value)
-    }
-}
-
-function virgula() {
-    if (tela.value.length == 1) {
-        tela.value = '';
-        erro();
-    } else {
-            return; 
+        pontoONOFF = false;
     }
 }
 
 function mudartema() {
+    tippyTema.style.display="none";
     if (tema.checked == false) {
         tema.checked = false;
         body.style.background = "#0A0A0A";
@@ -92,6 +174,7 @@ function mudartema() {
         for (i = 0, len = botao.length; i < len; i++) {
             botao[i].style.color = '#ffffff';
         }
+        TemaEscuro();
         console.log('Dark');
         console.log(tema)
     } else {
@@ -106,5 +189,7 @@ function mudartema() {
         for (i = 0, len = botao.length; i < len; i++) {
             botao[i].style.color = '#141414';
         }
+        //tippyTema.remove();
+        TemaClaro();
     }
 }
